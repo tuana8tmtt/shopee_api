d6054a3389bec5120f3b5e371eb06a8821c2afb8
tuana8tmtt <49206137+tuana8tmtt@users.noreply.github.com>
2021-08-31 18:58:16 +0700

    make it better

--- index.js
+++ index.js
@@ -5,6 +5,7 @@ var app = express();
 require('./routes/keywords')(app);
 
 
-app.listen(3000, function () {
-    console.log('running port 3000')
-})
+// app.listen(3000, function () {
+//     console.log('running port 3000')
+// })
+app.listen(process.env.PORT || 3000)
