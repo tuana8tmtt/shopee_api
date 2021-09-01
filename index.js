757af38feb02579675151fb105b1f91c2b55a0c2
tuana8tmtt <49206137+tuana8tmtt@users.noreply.github.com>
2021-09-02 00:00:41 +0700

    fix

--- index.js
+++ index.js
@@ -1,7 +1,13 @@
 const express = require('express');
 const morgan = require('morgan');
 var app = express();
-
+app.all('*', function (req, res, next) {
+    var origin = req.get('origin');
+    res.header('Access-Control-Allow-Origin', origin);
+    res.header("Access-Control-Allow-Headers", "X-Requested-With");
+    res.header('Access-Control-Allow-Headers', 'Content-Type');
+    next();
+}); `
 app.use(express.json());
 app.use(morgan('combined'));
 require('./routes/api/keywords')(app);
