6e4dde0bc9f06fb29a09921ed75cfe33912ec91f
tuana8tmtt <49206137+tuana8tmtt@users.noreply.github.com>
2021-09-01 16:13:53 +0700

    fix

--- index.js
+++ index.js
@@ -1,11 +1,13 @@
 const express = require('express');
+const morgan = require('morgan');
 var app = express();
 
 app.use(express.json());
+app.use(morgan('combined'));
 require('./routes/api/keywords')(app);
 require('./routes/api/products')(app);
 require('./routes/api/user')(app);
 
-app.listen(process.env.PORT || 5000, () => {
-    console.log('Express server listening on port 5000')
+app.listen(process.env.PORT || 3000, () => {
+    console.log('Express server listening on port 3000')
 });
