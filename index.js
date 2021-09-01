ea07458e837ddd5ac74ac34e8b88c9cb8c44980c
tuana8tmtt <49206137+tuana8tmtt@users.noreply.github.com>
2021-09-02 00:02:57 +0700

    fix

--- index.js
+++ index.js
@@ -7,7 +7,7 @@ app.all('*', function (req, res, next) {
     res.header("Access-Control-Allow-Headers", "X-Requested-With");
     res.header('Access-Control-Allow-Headers', 'Content-Type');
     next();
-}); `
+});
 app.use(express.json());
 app.use(morgan('combined'));
 require('./routes/api/keywords')(app);
